85f2197b91a5aeee1ab54d814818f65fe62b8d30
mmk2410 <marcelmichaelkapfer@yahoo.co.nz>
2015-07-05 18:05:56 +0200

    Localization; Add German

---
+++ lang/de.php
@@ -0,0 +1,7 @@
+<?php
+
+// LANGUAGE: GERMAN
+
+$LANG_Blogs_Of = "Blogs auf";
+$LANG_Check_out_this_blog = "Schau dir diesen Blog an:";
+$LANG_Check_out = "Schau dir das an:";
--- lang/en.php
+++ lang/en.php
@@ -1,7 +1,7 @@
 <?php
 
-// This is the default language file for PHP
+// LANGUAGE: ENGLISH
 
-$LANG_Blogs_Of = "Blogs of";
+$LANG_Blogs_Of = "Blogs on";
 $LANG_Check_out_this_blog = "Check out this blog";
 $LANG_Check_out = "Check out";
