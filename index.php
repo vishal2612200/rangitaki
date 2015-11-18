352182ae107060ac96590b751e025cc100b78dd1
mmk2410 <marcelmichaelkapfer@yahoo.co.nz>
2015-11-18 15:18:18 +0100

    Nav: Blogs highlighted when hovering

--- index.php
+++ index.php
@@ -10,30 +10,16 @@
 COPYRIGHT (c) 2015 mmk2410
 
 MIT License
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -->
 <html>
 <?php
 /**
  * Rangitaki PHP Blogging engine
+ * @category Blogging
+ * @package Rbe
+ * @author Marcel Kapfer (mmk2410) <marcelmichaelkapfer@yahoo.co.nz>
+ * @license MIT https://opensource.org/licenses/MIT
+ * @link https://marcel-kapfer.de/rangitaki
  */
 // Getting necessary php files
 date_default_timezone_set('UTC');
