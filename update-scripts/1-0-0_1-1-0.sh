fe81cb784e4e649f88b0431f64b286ca84bb30a7
mmk2410 <marcelmichaelkapfer@yahoo.co.nz>
2015-11-22 18:19:22 +0100

    Update script

---
+++ update-scripts/1-0-0_1-1-0.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Script for updating Rangitaki 1.0.0 to 1.1.0
+# WARNING: Rangitaki 1.1.0 is for testing purposes only
+echo "WARNING: You're about to install a testing version of Rangitaki."
+read -r -p "Do you want to continue? (y/N) " answer
+if [[ "$answer" == "y" || "$answer" == "Y"  ]]; then
+    # Getting the up to date version
+    git clone https://github.com/mmk2410/rangitaki/ ./rbe-new
+    # Creating temporary directory
+    mkdir rbe-tmp
+    # Updating rcc
+    cp ./rcc/password.php ./rbe-tmp/
+    rm -rf ./rcc
+    mv ./rbe-new/rcc ./
+    rm ./rcc/password.php
+    mv ./rbe-tmp/password.php ./rcc/
+    # Updating ressources
+    rm -rf ./res
+    mv ./rbe-new/res ./
+    # Updating index.php
+    rm ./index.php
+    mv ./rbe-new/index.php ./
+    # Updating themes
+    rm ./themes/material-light.css
+    mv ./rbe-new/themes/material-light.css ./themes
+    # cleaning up
+    rm -rf ./rbe-new
+    rm -rf ./rbe-tmp
+fi
