0baffcc5cf91b428ab1276d10d83f17ebc6c8870
Marcel Kapfer (mmk2410) <marcelmichaelkapfer@yahoo.co.nz>
2016-06-03 15:25:59 +0200

    Check first if value exists

--- rcc/ssl.php
+++ rcc/ssl.php
@@ -3,7 +3,7 @@
 // License: MIT License
 // SSL Verification
 
-if ($settings["rcc"]["debug"] != "on") {
+if (isset($settings["rcc"]["debug"]) && $settings["rcc"]["debug"] != "on") {
     if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
         header('HTTP/1.1 400 Bad Request');
         exit();
